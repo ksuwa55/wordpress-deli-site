edf951890161370db1aa603372153c333ac1921f
ksuwa55 <klklklkl063055@gmail.com>
2025-09-12 00:12:26 +0900

    apply google fonts and change fonts

--- wordpress/wp-content/themes/kitchenstudio-theme/functions.php
+++ wordpress/wp-content/themes/kitchenstudio-theme/functions.php
@@ -3,6 +3,7 @@
 add_action('wp_enqueue_scripts', function() {
   wp_enqueue_style(
     'kitchenstudio-theme-style',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Noto+Sans+JP:wght@400;500;700&display=swap',
     get_stylesheet_uri(),
     [],
     wp_get_theme()->get('Version')
